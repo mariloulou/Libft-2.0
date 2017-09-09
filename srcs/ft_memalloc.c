7ff744fb5ab5ccd3343e03a2587e9f20ccd6ee54
Marilou CASSAR <mcassar@e2r2p18.42.fr>
2017-09-09 14:22:09 +0200

    ft_strmap, ft_strmapi & norm

--- srcs/ft_memalloc.c
+++ srcs/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mcassar <mcassar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/06 15:49:56 by mcassar           #+#    #+#             */
-/*   Updated: 2017/09/06 16:01:47 by mcassar          ###   ########.fr       */
+/*   Updated: 2017/09/09 14:21:08 by mcassar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memalloc(size_t size)
 {
-	char *s1;
-	void *s2;
-	size_t i;
+	char	*s1;
+	void	*s2;
+	size_t	i;
 
 	i = 0;
 	s2 = (void *)malloc(sizeof(*s1) * size);
--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -1,18 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcassar <mcassar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/06 14:41:43 by mcassar           #+#    #+#             */
-/*   Updated: 2017/09/09 14:20:55 by mcassar          ###   ########.fr       */
+/*   Created: 2017/09/09 14:14:10 by mcassar           #+#    #+#             */
+/*   Updated: 2017/09/09 14:21:25 by mcassar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
+#include "../headers/libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
+	char	*ret;
+	int		i;
+
+	if (!s || !f)
+		return (0);
+	i = 0;
+	ret = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (ret == NULL)
+		return (0);
+	while (s[i])
+	{
+		ret[i] = f(i, s[i]);
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
 }
